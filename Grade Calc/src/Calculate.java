d171b3a8804558320316ee0ed9b54bc189a16ad0
Christian Kovacs <66984388+ckovacs1@users.noreply.github.com>
2021-05-20 21:27:42 -0400

    Update Calculate.java

--- Grade Calc/src/Calculate.java
+++ Grade Calc/src/Calculate.java
@@ -1,13 +1,37 @@
+/**
+ *Uses the grade class to calculate the grade using the data from the grade
+ */
 public class Calculate {
 
     private Grade grade;
 
+    /**
+     * Constructs the calculation
+     * @param grade
+     */
     public Calculate(Grade grade){
         this.grade = grade;
     }
 
-
+    /**
+     * takes the data from the grade and calculates the percent needed on the final to reach the goal
+     * @return
+     */
     public double calcGrade(){
+
+        /*The grade is calculated using numbers between 0 and 1 so any number greater than 1 will
+        * be converted into a number between 0 and 1
+        * */
+        if (grade.getFinPercent() > 1){
+            grade.setFinPercent(grade.getFinPercent() / 100);
+        }
+        if (grade.getCurrent() > 1){
+            grade.setCurrent(grade.getCurrent() / 100);
+        }
+        if (grade.getGoal() > 1){
+            grade.setGoal(grade.getGoal() / 100);
+        }
+
 
         double gradeReq = (grade.getGoal() -( ( 1 - grade.getFinPercent()) * grade.getCurrent())) / grade.getFinPercent();
         return gradeReq;
