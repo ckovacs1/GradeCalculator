fce58244c02f269c0720973b99018c159a1a1c87
Christian Kovacs <66984388+ckovacs1@users.noreply.github.com>
2021-05-24 22:58:50 -0400

    Update Main.java

--- Grade Calc/src/Main.java
+++ Grade Calc/src/Main.java
@@ -1,18 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Formatter;
 
 /**
  * @author Christian Kovacs
  * WIP Not finished
- * 
+ *
  */
-public class Main {
+public class Main implements ActionListener {
+
+    private static JPanel panel;
+    private static JFrame frame;
+    private static JLabel currentLabel;
+    private static JTextField currentText;
+    private static JLabel goalLabel;
+    private static JTextField goalText;
+    private static JLabel finalLabel;
+    private static JTextField finalText;
+    private static JButton button;
+    private static JLabel output;
 
     //https://www.youtube.com/watch?v=iE8tZ0hn2Ws
     public static void main(String[] args){
 
         //UI SetUp
-        JPanel panel = new JPanel();
-        JFrame frame = new JFrame();
+        panel = new JPanel();
+        frame = new JFrame();
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
@@ -20,65 +34,92 @@ public class Main {
 
 
         //Creates the label for current grade
-        JLabel currentLabel = new JLabel("Current Grade: ");
+        currentLabel = new JLabel("Current Grade: ");
         currentLabel.setBounds(10,20, 130, 25);
         panel.add(currentLabel);
 
         //Creates the textbox for the current grade
-        JTextField currentText = new JTextField();
+        currentText = new JTextField();
         currentText.setBounds(130,20,165,25);
         panel.add(currentText);
 
 
         //Creates Goal Label
-        JLabel goalLabel = new JLabel(("Grade Goal: "));
+        goalLabel = new JLabel(("Grade Goal: "));
         goalLabel.setBounds(10,90, 150, 25);
         panel.add(goalLabel);
 
         //Creates Goal TextBox
-        JTextField goalText = new JTextField();
+        goalText = new JTextField();
         goalText.setBounds(130,90,165,25);
         panel.add(goalText);
 
 
         //Creates Final Weight Label
-        JLabel finalLabel = new JLabel(("Final Exam Weight: "));
+        finalLabel = new JLabel(("Final Exam Weight: "));
         finalLabel.setBounds(10,160, 150, 25);
         panel.add(finalLabel);
 
         //Creates Final Weight Textbox
-        JTextField finalText = new JTextField();
+        finalText = new JTextField();
         finalText.setBounds(130,160,165,25);
         panel.add(finalText);
 
 
         //Creates and Adds the Enter Button
-        JButton button = new JButton("Enter");
+        button = new JButton("Enter");
         button.setBounds(215,220,79,25);
         panel.add(button);
 
+        button.addActionListener(new Main());
 
         frame.setVisible(true);
 
 
+        output = new JLabel((""));
+        output.setBounds(10,230, 150, 25);
+        panel.add(output);
 
 
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        //creates a new grade object
         Grade name = new Grade();
 
-        //number input here
-        //TODO add UI to this
+        //gets the inputs from the text boxes in the UI
+        String current = currentText.getText();
+        String goal    = goalText.getText();
+        String finPerc = finalText.getText();
 
-        name.setCurrent(50);
-        name.setGoal(60);
-        name.setFinPercent(25);
+        //Sets them to doubles
+        double curD = Double.parseDouble(current);
+        double goaD = Double.parseDouble(goal);
+        double finD = Double.parseDouble(finPerc);
 
+        //sets the inputs to the grade object
+        name.setCurrent(curD);
+        name.setGoal(goaD);
+        name.setFinPercent(finD);
+
+        //creates calculation
         Calculate name1 = new Calculate(name);
 
-
+        //calculates final grade
         double finalG =  (name1.calcGrade() * 100);
-        //System.out.printf("%.2f %n", finalG);
-        
+
+        //formats answer to 2 decimal places
+        Formatter formatter = new Formatter();
+        formatter.format("%.2f", finalG);
+
+
+        String finalOut = String.valueOf(formatter);
+
+        output.setText("You need: " + finalOut + "%");
+
     }
 
 }
